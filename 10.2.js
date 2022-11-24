959c1c9ae90c5d3a938b103ce7dad902611c3cbe
OxanaSidorova <106552165+OxanaSidorova@users.noreply.github.com>
2022-11-24 21:59:24 +0000

    added test 10.2

---
+++ 10.2.js
@@ -0,0 +1,72 @@
+let students =
+[
+    [
+      { "name": "Ivan", "score": 35, "date": "2022-10-11" },
+      { "name": "Maria", "score": 5, "date": "2022-10-10" },
+      { "name": "Olga", "score": 0, "date": "" },
+      { "name": "Stepan", "score": 35, "date": "2022-10-12" },
+      { "name": "Oleg", "score": 9, "date": "2022-10-01" },
+      { "name": "Zanna", "score": 15, "date": "2022-10-11" }
+    ],
+    [
+      { "name": "Margo", "score": 0, "date": "2022-10-11" },
+      { "name": "Natalia", "score": 25, "date": "2022-10-10" },
+      { "name": "Marina", "score": 25, "date": "2022-10-01" },
+      { "name": "Dmitry", "score": 25, "date": "2022-10-12" },
+      { "name": "Denis", "score": 0, "date": "2022-10-02" },
+      { "name": "Vadimyr", "score": 1, "date": "2022-10-11" }
+    ],
+    [
+      { "name": "Irina", "score": 0, "date": "2022-10-11" },
+      { "name": "Vasily", "score": 0, "date": "2022-10-10" },
+      { "name": "David", "score": 0, "date": "2022-10-11" },
+      { "name": "Kristina", "score": 0, "date": "2022-10-12" },
+      { "name": "Varvara", "score": 0, "date": "2022-10-01" },
+      { "name": "Tanya", "score": 0, "date": "2022-10-11" }
+    ]
+  ]
+
+  let studentList =[]
+   for(let student of students){
+    student.forEach((element)=>studentList.push(element))
+   }
+        let sortedNewArray=studentList.sort((a,b)=>{
+            return a.score>b.score&&Date(a.date)>Date(b.date)
+
+        })
+        console.log(sortedNewArray)
+
+        let topStudent = sortedNewArray.reverse().pop()
+        topStudent = { name: 'Ivan', score: 35, date: '2022-10-11' }
+
+// console.log(topStudent)
+        
+        console.log( `Well done ${topStudent.name} you've earned ${topStudent.score} points`)
+
+        module.export = sortedNewArray
+
+
+
+
+    //      topStudent = {name:"Ivan",score:35,date:"2022-10-11"}
+    //      console.log(topStudent)
+
+
+
+
+
+    //              if(maxScore=Math.max(maxScore, student.score)
+    //                 // console.log([Math.max(...newArray)])
+                    
+                
+                 
+        
+  
+    //         maxScore=Math.max (...players.scorePoints)
+    //          maxScore= Math.max(maxScore, student.score) 
+       
+
+    // }
+
+    //  console.log(`Congradulations studen ${studentName} you has the max score of points - ${maxScore}`)
+
---
+++ 10.2.test.js
@@ -0,0 +1,18 @@
+const sortedNewArray=require('./10.2')
+
+describe("test if Ivan has the max score",()=>{
+    test('if there is a student name Ivan',()=> {
+        
+    expect(sortedNewArray).toContain('Ivan');
+    // });
+    // test ('if there is student name Ivan?', () => {
+    expect(new Set(sortedNewArray)).toContain('Ivan');
+    }
+    )}
+)
+    describe ("Ivan has the max score",()=>{
+    test('if student Ivan has score 35',()=> {
+        expect(new Set(sortedNewArray)).toContain('35')
+    }
+    )}
+    )
